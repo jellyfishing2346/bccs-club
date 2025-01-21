580b23c5abac5efc097d926b69645d4e6ed59084
jellyfishing2346 <108194702+jellyfishing2346@users.noreply.github.com>
2025-01-20 21:05:18 -0500

    Added a pages directory to the root directory

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// pages/_app.js
+import '@/styles/globals.css'; // Make sure this points to your global styles if you have them
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
